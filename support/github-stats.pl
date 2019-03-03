30f0d68e77b91068179a3e43490b8c9f97a00e69
Stuart Low <stuart.low@data61.csiro.au>
2019-03-04 10:16:28 +1100

    Add user exclusions and repo masking + perltidy

--- support/github-stats.pl
+++ support/github-stats.pl
@@ -4,22 +4,28 @@ use Net::GitHub;
 use Data::Dumper;
 use Text::ASCIITable;
 use strict;
-$|=1;
+$| = 1;
 
 #####
 # Very quick script to retrieve, parse and summarise github stats
 # No warranty implied!
 #####
-my $orgName = 'ConsumerDataStandardsAustralia';
-my $topUserCount = 10;
-
-
-my $github = Net::GitHub->new(
-	access_token => $ENV{'TOKEN'}
+my $orgName           = 'ConsumerDataStandardsAustralia';
+my $topUserCount      = 10;
+my @excludedUsersList = (
+    'csirostu',   'perlboy',   'JamesMBligh', 'ajmcmiddlin',
+    'nghamilton', 'benkolera', 'enemeth79',   'lukepopp',
+    'ellenbroad', 'csirocdr'
 );
+my @repositoryList = ( 'standards', 'infosec' );
+my $specificRepositories = 0;
+
+die "Must supply TOKEN variable" if !$ENV{'TOKEN'};
+
+my $github = Net::GitHub->new( access_token => $ENV{'TOKEN'} );
 
 my $issueApi = $github->issue;
-my $repoApi = $github->repos;
+my $repoApi  = $github->repos;
 
 my @repos = $repoApi->list;
 
@@ -28,31 +34,47 @@ my %repoMap;
 foreach my $oneRepo (@repos) {
     my $repoName = $oneRepo->{'name'};
 
+    if ( $specificRepositories && !grep( /^$repoName$/i, @repositoryList ) ) {
+        next;
+    }
+
     $repoMap{$repoName}{'watchers'} = $oneRepo->{'watchers_count'};
 
     # Set some defaults
-    $github->set_default_user_repo($orgName, $repoName);
-    $issueApi->set_default_user_repo($orgName, $repoName);
-    $repoApi->set_default_user_repo($orgName, $repoName);
+    $github->set_default_user_repo( $orgName, $repoName );
+    $issueApi->set_default_user_repo( $orgName, $repoName );
+    $repoApi->set_default_user_repo( $orgName, $repoName );
 
     print "Processing $repoName: ";
 
-    while(my $oneIssue = $issueApi->next_repos_issue($orgName, $repoName, { state => 'all' })) {
+    while ( my $oneIssue =
+        $issueApi->next_repos_issue( $orgName, $repoName, { state => 'all' } ) )
+    {
         print "I" . $oneIssue->{'number'} . " ";
         $repoMap{$repoName}{'issue_count'}++;
-        if($oneIssue->{'state'} eq 'open') {
+        if ( $oneIssue->{'state'} eq 'open' ) {
             $repoMap{$repoName}{'open_issue_count'}++;
         }
         $repoMap{$repoName}{'issue_interactions'} += $oneIssue->{'comments'};
-        chomp($oneIssue->{'user'}->{'login'});
-        $repoMap{$repoName}{'contributors'}{$oneIssue->{'user'}->{'login'}}++;
+        chomp( $oneIssue->{'user'}->{'login'} );
+        my $searchUser = $oneIssue->{'user'}->{'login'};
+        if ( !grep( /^$searchUser$/i, @excludedUsersList ) ) {
+            $repoMap{$repoName}{'contributors'}
+              { $oneIssue->{'user'}->{'login'} }++;
+        }
 
         # Work through comments
-        if($oneIssue->{'comments'} > 0) {
-            while(my $oneComment = $issueApi->next_comment($oneIssue->{'number'})) {
-                    print "C ";
-                    chomp($oneComment->{'user'}->{'login'});
-                    $repoMap{$repoName}{'contributors'}{$oneComment->{'user'}->{'login'}}++;
+        if ( $oneIssue->{'comments'} > 0 ) {
+            while ( my $oneComment =
+                $issueApi->next_comment( $oneIssue->{'number'} ) )
+            {
+                print "C ";
+                chomp( $oneComment->{'user'}->{'login'} );
+                my $searchUser = $oneComment->{'user'}->{'login'};
+                if ( !grep( /^$searchUser$/i, @excludedUsersList ) ) {
+                    $repoMap{$repoName}{'contributors'}
+                      { $oneComment->{'user'}->{'login'} }++;
+                }
             }
 
         }
@@ -60,11 +82,15 @@ foreach my $oneRepo (@repos) {
 
     print " ";
 
-    while(my $oneCommit = $repoApi->next_commit($orgName, $repoName)) {
+    while ( my $oneCommit = $repoApi->next_commit( $orgName, $repoName ) ) {
         print "S";
-        chomp($oneCommit->{'author'}->{'login'});
-        if($oneCommit->{'author'}->{'login'} ne '') {
-            $repoMap{$repoName}{'contributors'}{$oneCommit->{'author'}->{'login'}}++;
+        chomp( $oneCommit->{'author'}->{'login'} );
+        my $searchUser = $oneCommit->{'author'}->{'login'};
+        if ( $oneCommit->{'author'}->{'login'} ne ''
+            && !grep( /^$searchUser$/i, @excludedUsersList ) )
+        {
+            $repoMap{$repoName}{'contributors'}
+              { $oneCommit->{'author'}->{'login'} }++;
         }
         $repoMap{$repoName}{'commit_count'}++;
     }
@@ -74,36 +100,37 @@ foreach my $oneRepo (@repos) {
 }
 
 my $totalInteractions = 0;
-my $totalIssues = 0;
+my $totalIssues       = 0;
 my $totalRepositories = keys %repoMap;
 my %userMap;
 my $repoTable = Text::ASCIITable->new();
-$repoTable->setCols(['Repo Name', 'Issues', 'Open Issues', 'Comments', 'Users', 'Commits']);
-foreach my $oneRepo (keys %repoMap) {
-    $totalIssues += $repoMap{$oneRepo}{'issue_count'};
+$repoTable->setCols(
+    [ 'Repo Name', 'Issues', 'Open Issues', 'Comments', 'Users', 'Commits' ] );
+foreach my $oneRepo ( sort keys %repoMap ) {
+    $totalIssues       += $repoMap{$oneRepo}{'issue_count'};
     $totalInteractions += $repoMap{$oneRepo}{'issue_interactions'};
-    my @contributorList = keys %{$repoMap{$oneRepo}{'contributors'}};
+    my @contributorList = keys %{ $repoMap{$oneRepo}{'contributors'} };
     foreach my $thisOneUser (@contributorList) {
-        $userMap{$thisOneUser} += $repoMap{$oneRepo}{'contributors'}{$thisOneUser};
+        $userMap{$thisOneUser} +=
+          $repoMap{$oneRepo}{'contributors'}{$thisOneUser};
     }
 
-    my %contributorsHash = %{$repoMap{$oneRepo}{'contributors'}};
+    my %contributorsHash = %{ $repoMap{$oneRepo}{'contributors'} };
     my $contributorCount = keys %contributorsHash;
 
     $repoTable->addRow(
         [
             $oneRepo,
-            $repoMap{$oneRepo}{'issue_count'} || 0,
-            $repoMap{$oneRepo}{'open_issue_count'} || 0,
+            $repoMap{$oneRepo}{'issue_count'}        || 0,
+            $repoMap{$oneRepo}{'open_issue_count'}   || 0,
             $repoMap{$oneRepo}{'issue_interactions'} || 0,
-            $contributorCount || 0,
-            $repoMap{$oneRepo}{'commit_count'} || 0
+            $contributorCount                        || 0,
+            $repoMap{$oneRepo}{'commit_count'}       || 0
         ]
     );
 }
 
 my $totalUniqueUsers = keys %userMap;
-
 
 #my @issues = $issueApi->issues();
 #print Dumper(\@issues);
@@ -116,10 +143,10 @@ print qq~
 ------------------------------------------------
 ~;
 
-printf("Total Repositories: %d\n", $totalRepositories);
-printf("Total Issues: %d\n", $totalIssues);
-printf("Total Interaction Count: %d\n", $totalInteractions);
-printf("Total Unique Contributor Count: %d\n", $totalUniqueUsers);
+printf( "Total Repositories: %d\n",             $totalRepositories );
+printf( "Total Issues: %d\n",                   $totalIssues );
+printf( "Total Interaction Count: %d\n",        $totalInteractions );
+printf( "Total Unique Contributor Count: %d\n", $totalUniqueUsers );
 
 print qq~
 ------------------------------------------------
@@ -137,25 +164,19 @@ print qq~
 
 my @sortedUsers = sort { $userMap{$a} <=> $userMap{$b} } keys %userMap;
 my $userTable = Text::ASCIITable->new();
-$userTable->setCols(['Username', 'Contributions']);
+$userTable->setCols( [ 'Username', 'Contributions' ] );
 
 my $topCount = 0;
-foreach my $userName (reverse @sortedUsers) {
-    if($topCount > $topUserCount) { last; }
+foreach my $userName ( reverse @sortedUsers ) {
+    if ( $topCount > $topUserCount ) { last; }
 
-    if($userName eq '') { next; }
+    if ( $userName eq '' ) { next; }
 
-    $userTable->addRow(
-        [
-            $userName,
-            $userMap{$userName}
-        ]
-    );
+    $userTable->addRow( [ $userName, $userMap{$userName} ] );
 
     $topCount++;
 
 }
 
 print $userTable;
-
 
