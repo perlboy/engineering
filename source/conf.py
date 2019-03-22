e4b05484da32392301e497b9436a2407ee9baf04
Stuart Low <stuart.low@data61.csiro.au>
2019-03-22 17:20:29 +1100

    Brand police

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'CDR Engineering'
+project = 'CDS Engineering'
 copyright = '2019, CSIRO Data61'
 author = 'CSIRO Data61'
 
@@ -117,7 +117,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'CDREngineeringdoc'
+htmlhelp_basename = 'CDSEngineeringdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -144,7 +144,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'CDREngineering.tex', 'CDR Engineering Documentation',
+    (master_doc, 'CDSEngineering.tex', 'CDS Engineering Documentation',
      'CSIRO Data61', 'manual'),
 ]
 
@@ -154,7 +154,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'cdrengineering', 'CDR Engineering Documentation',
+    (master_doc, 'cdrengineering', 'CDS Engineering Documentation',
      [author], 1)
 ]
 
@@ -165,8 +165,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'CDREngineering', 'CDR Engineering Documentation',
-     author, 'CDREngineering', 'One line description of project.',
+    (master_doc, 'CDSEngineering', 'CDS Engineering Documentation',
+     author, 'CDSEngineering', 'One line description of project.',
      'Miscellaneous'),
 ]
 
