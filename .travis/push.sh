55b8a8d81925b3930620022902d2ce050a4513fa
Stuart Low <Stuart.Low@data61.csiro.au>
2019-03-25 12:39:11 +1100

    Move nuking into push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,8 +5,16 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
+strip_repository() {
+  git checkout -b gh-pages	
+  rm -fr *
+  touch .nojekyll
+  docker run $IMAGE_NAME tar -c -C /opt/engineering/build html | tar x
+  cp -Rp html/ ./
+  rm -fr html
+}
+
 commit_website_files() {
-  git checkout -b gh-pages
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
@@ -18,6 +26,7 @@ upload_files() {
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
      setup_git
+     strip_repository
      commit_website_files
      upload_files
 fi
